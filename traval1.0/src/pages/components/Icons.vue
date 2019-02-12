15eef7c2fda3c7cd93e1759b06a2ae9561c58bf0
lin git <727344504@qq.com>
2019-02-12 08:33:05 +0800

    index-icons

---
+++ traval1.0/src/pages/components/Icons.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="icons">
+    <!-- 当图标有9个的时候，可以像轮播图一样的切换 -->
+    <swiper>  
+      <swiper-slide v-for="(page,index) of pages" :key="index">   
+    <div class="icon" 
+         v-for='item of page'
+         :key="item.id"
+         >
+      <div class="icon-img">
+        <img class="icon-img-content" :src="item.imgUrl">
+        <p class="icon-desc">{{item.desc}}</p>
+      </div>
+    </div>
+     </swiper-slide>   
+    </swiper> 
+  </div>
+</template>
+<script>
+import img from "../../assets/img/j1.png"
+export default {
+  name: "HomeIcons",
+  data: function() {
+    return {
+        iconList:[{
+         id:'0001',
+         imgUrl:img,
+         desc:'景点门票'
+        },{
+         id:'0002',
+         imgUrl:img,
+         desc:'景点门票'
+        },{
+         id:'0003',
+         imgUrl:img,
+         desc:'景点门票'
+        },{
+         id:'0004',
+         imgUrl:img,
+         desc:'景点门票'
+        },{
+         id:'0005',
+         imgUrl:img,
+         desc:'景点门票'
+        },{
+         id:'0006',
+         imgUrl:img,
+         desc:'景点门票'
+        },{
+         id:'0007',
+         imgUrl:img,
+         desc:'景点门票'
+        },{
+         id:'0008',
+         imgUrl:img,
+         desc:'景点门票'
+        },{
+         id:'0009',
+         imgUrl:img,
+         desc:'景点门票'
+        }]
+    };
+  },
+  computed:{
+    pages (){
+       const pages=[];
+       this.iconList.forEach((item,index)=>{
+         /* 显示的是0里边放了8个,在1里边放了一个 */
+          const page=Math.floor(index/8);
+          if(!pages[page]){
+            pages[page]=[];
+          }
+          pages[page].push(item)
+       })
+       return pages
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import "~@/assets/styles/varibles.scss";
+/deep/.swiper-container{
+  height: 0;
+  padding-bottom: 50%;
+  // background-color: pink;
+}
+  .icon {
+    overflow: hidden;
+    float: left;
+    width: 25%;
+    padding-bottom: 25%;
+    // background-color: red;
+    position: relative;
+    height: 0;
+    .icon-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0.44rem;
+      box-sizing: border-box;
+      padding: 0.1rem;
+      .icon-img-content {
+        height: 100%;
+        display: block;
+        margin: 0 auto;
+      }
+    }
+    .icon-desc {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      line-height: 0.44rem;
+      height: 0.44rem;
+      color: $darkTextColor;
+      text-align: center;
+      /* 文字多加上省略号的 */
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+  }
+</style>
--- traval1.0/src/pages/home/home.vue
+++ traval1.0/src/pages/home/home.vue
@@ -4,6 +4,7 @@
    <!-- 下边定义的是大写，在这里写的是小写的，他会自动转换 -->
    <home-header></home-header>
    <home-swiper></home-swiper>
+   <home-icons></home-icons>
 </div>
 </template>
 
@@ -12,13 +13,17 @@ import HomeHeader from '../components/Header'
 /* 引入的轮播图的组件 */
 import HomeSwiper from '../components/Swiper'
 /* export defaul用于对外输出本模块（一个文件可以理解为一个模块）变量的接口 */
+import HomeIcons from '../components/Icons'
+/* 引入图标区域文件 */
 export default {
  name:'home',
  /* home.vue是首页的大组件*/
  components:{
      HomeHeader,
      /* 如果是es6的语法的话，可以直接写上HomeHeader */
-     HomeSwiper
+     HomeSwiper,
+     /* 引入图标区域文件 */
+     HomeIcons
  } 
 }
 </script>
