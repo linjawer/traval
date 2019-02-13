b862509250af4e4207d1f4498e5ba54a7ab0b12f
lin git <727344504@qq.com>
2019-02-13 19:40:23 +0800

    recommend

---
+++ traval1.0/src/pages/components/Weekend.vue
@@ -0,0 +1,80 @@
+<template>
+<div>
+    <div class="recommend-title">周末去哪儿</div>
+    <ul>
+        <li class="item border-bottom" v-for="item of recommendList" :key='item.id'>
+            <div class="item-img-wrappers">
+               <img class="item-img-wrapper" :src="item.imgurl">
+            </div>
+            <div class="item-info">
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+                <!-- <button class="item-button">查看详情</button> -->
+            </div>
+        </li>
+    </ul>
+</div>    
+</template>
+
+<script>
+import img from "../../assets/img/123.jpg"
+export default {
+    name:'HomeWeekend',
+    data (){
+        return {
+           recommendList:[{
+               id:'0001',
+               imgurl:img,
+               title:'大连圣亚海洋世界',
+               desc:'浪漫广州首站,浪漫的海洋主题公园'
+           },
+           {
+               id:'0002',
+               imgurl:img,
+               title:'大连圣亚海洋世界',
+               desc:'浪漫广州首站,浪漫的海洋主题公园'
+           },
+           {
+               id:'0003',
+               imgurl:img,
+               title:'大连圣亚海洋世界',
+               desc:'浪漫广州首站,浪漫的海洋主题公园'
+           }]    
+        }
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+  .recommend-title{
+    line-height: .8rem;//20px
+    background-color: #eee;
+    text-indent:.2rem;
+  }
+  .item{
+    .item-img-wrappers{
+        height: 0;
+        overflow: hidden;
+        /* 图片的宽高比 */
+        padding-bottom: 33.9%;
+        .item-img-wrapper{
+          width: 100%;
+      }
+      }
+ 
+      .item-info{
+        //   flex: 1;
+          padding: .1rem;
+          .item-title{
+              line-height: .54rem;
+              font-size: .32rem;
+          }
+          .item-desc{
+              line-height: .4rem;
+              color: #ccc;
+          }
+      }
+  }
+</style>
+
+
--- traval1.0/src/pages/home/home.vue
+++ traval1.0/src/pages/home/home.vue
@@ -5,6 +5,8 @@
    <home-header></home-header>
    <home-swiper></home-swiper>
    <home-icons></home-icons>
+   <home-recommend></home-recommend>
+   <home-weekend></home-weekend>
 </div>
 </template>
 
@@ -15,6 +17,9 @@ import HomeSwiper from '../components/Swiper'
 /* export defaul用于对外输出本模块（一个文件可以理解为一个模块）变量的接口 */
 import HomeIcons from '../components/Icons'
 /* 引入图标区域文件 */
+import HomeRecommend from '../components/Recommend'
+/* 引入周末组件 */
+import HomeWeekend from '../components/Weekend'
 export default {
  name:'home',
  /* home.vue是首页的大组件*/
@@ -23,7 +28,11 @@ export default {
      /* 如果是es6的语法的话，可以直接写上HomeHeader */
      HomeSwiper,
      /* 引入图标区域文件 */
-     HomeIcons
+     HomeIcons,
+     /* 引入热销的样式 */
+     HomeRecommend,
+     /* 引入周末组件 */
+     HomeWeekend
  } 
 }
 </script>
